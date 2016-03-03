04f6d858404f7cdbcf3bd1bba818a45de261c0a3
andershanssen <andershanssen92@gmail.com>
2016-03-03 16:23:31 +0100

    Update MessageParser.py

--- Skeleton/Client/MessageParser.py
+++ Skeleton/Client/MessageParser.py
@@ -8,7 +8,7 @@ class MessageParser():
             'error': self.parse_error,
             'info': self.parse_info,
             'message': self.parse_message,
-            'history': self.parse_history	
+            'history': self.parse_history   
         }
 
     def parse(self, payload):
@@ -17,16 +17,23 @@ class MessageParser():
         if payload['response'] in self.possible_responses:
             return self.possible_responses[payload['response']](payload)
         else:
-            # Response not valid
+            return
 
     def parse_error(self, payload):
-        return payload['content'];
+        print "parse_error: %s" % payload['content']
+        #return payload['content'];
     
     def parse_info(self, payload):
-        return payload['content'];
+        print "parse_info: %s" % payload['content']
+        #return payload['content'];
 
     def parse_message(self, payload):
-        return payload['timestamp'] + " " + payload['sender'] + ": " payload['content']
+        print "%s %s : %s" % (payload['timestamp'], payload['sender'],  payload['content'])
+        # Timestamp - from - msg
 
     def parse_history(self, payload): 
-        return payload['content'];
+        print "This is what you missed since last time"
+
+        for payload in payload['content']:
+            history = json.loads(payload)
+            self.parse_message(history)
